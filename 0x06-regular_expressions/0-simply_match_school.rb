d6d4402b5e9410b375bd86ff1ef0a4db1186aa28
2012Inga <inganathi2001@gmail.com>
2024-01-30 02:22:58 -0800

    simply-match_update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Get the argument passed to the script
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Match the pattern in the input string
-match = input_string.match(pattern)
-
-# Output the matched result
-puts match ? match[0] : ""
+puts ARGV[0].scan(/School/).join
